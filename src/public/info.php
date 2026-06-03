95bf866c8d29848b5bcd211459f793c1c5c33d59
Franco Ruggieri <fran_ruggi@hotmail.com>
2026-06-03 00:18:31 -0300

    testeo conexion db

--- src/public/info.php
+++ src/public/info.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<?php
+// TEMPORAL - borrar después de debuggear
+$host = 'localhost\SQLEXPRESS';
+$dsn  = "sqlsrv:Server={$host};Database=MuOnline";
+
+try {
+    $pdo = new PDO($dsn, 'sa', 'fr_58#R_4fw$%olZs!455', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "Conexión OK";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
